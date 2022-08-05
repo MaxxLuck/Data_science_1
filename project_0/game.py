10aa19aa952594e11c7b9c90f449cab2e1e78005
Maxxluck <m.leo.luck@gmail.com>
2022-08-05 12:21:37 +0300

    clean not used files

--- project_0/game.py
+++ project_0/game.py
@@ -1,10 +1,10 @@
 """Игра "угадай число"    """
 import numpy as np
-number= np.random.randint(1,101)
+number= np.random.randint(1,21)
 count=0
 while True:
     count+=1
-    predict_number=int(input('Угадай число от 1 до 100 '))
+    predict_number=int(input('Угадай число от 1 до 20 '))
     if predict_number>number:
         print('Число должно быть меньше')
     elif predict_number<number:
