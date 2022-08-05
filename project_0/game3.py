1162ad26aa875f42f618b9991024bda910771e7d
Maxxluck <m.leo.luck@gmail.com>
2022-08-05 14:02:20 +0300

    debug num100

--- project_0/game3.py
+++ project_0/game3.py
@@ -11,10 +11,11 @@ def random_predict(number:int=np.random.randint(1, 101)) -> int:
     """
     count = 0 # задаем счетчик
     min_num = 1
-    max_num = 100
+    max_num = 101
     
     while True:
         count +=1
+        # print('Число:', number, '---', 'Попытка:', count) # подсказал ментор как поймать лучше ошибку. Оказалось, что алгоритм  "зависал" на числе 100)
         predict_num = int((min_num + max_num) / 2)  # загадываем число как среднее между 
                                                     # минимальным и максимальным и отбрасываем дробную часть
         
@@ -25,9 +26,9 @@ def random_predict(number:int=np.random.randint(1, 101)) -> int:
             min_num = predict_num
             
         else:
-            # print(f'Алгоритм рассчитал число {number} за {count} попыток') #активировал данную строку, 
-            # если надо посмотреть все ли числа вылавливает алгоритм 
-            # (сказали, что некоторые алгоритмы могут не ловить числа 1 и 99)
+            # print(f'Алгоритм рассчитал число {number} за {count} попыток') # пробовал сам поймать ошибку, 
+            # (после 233 итерации алгоритм зависал).
+            # активировал данную строку,  если надо посмотреть все ли числа вылавливает алгоритм 
             break
     return count
 
@@ -44,13 +45,10 @@ def score_game(random_predict) -> int:
     count_ls=[] # список для записи количества попыток
     np.random.seed(1)
     random_array = np.random.randint(1, 101, size=(1000))
-    count1 = 0
-    
+        
     for number in random_array:
         count_ls.append(random_predict(number))
-        count1+=1
-        # print(f"итерация номер {count1}") #активировал данную строку чтобы посмотреть количество итераций.
-        # После 233 итерации работа алгоритма останавливалась.     
+            
     score = int(np.mean(count_ls))
     print(f'Наш алгоритм угадывает число за {score} попыток')
     return score
